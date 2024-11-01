71ab82cbac5909ba72a1911887b405293f86e5a5
Nelman25 <jonelvillaver735@gmail.com>
2024-11-02 01:59:12 +0800

    rewrite exercise 3.6 solution

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-
 app.use(express.json());
 
+const date = new Date();
 let persons = [
 	{
 		id: "1",
@@ -26,57 +26,66 @@ let persons = [
 	},
 ];
 
-const date = new Date().toString();
+const generateId = () => {
+	const randomId = Math.random() * 100;
+	return String(randomId);
+};
 
-app.get("/api/persons", (request, response) => {
-	response.json(persons);
+const nameAlreadyExist = (name) => {
+	const names = [...persons.map((person) => person.name.trim().toLowerCase())];
+	return names.includes(name.toLowerCase().trim());
+};
+
+app.get("/api/persons", (req, res) => {
+	res.json(persons);
 });
 
-app.get("/info", (request, response) => {
-	response.send(
-		`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`
+app.get("/info", (req, res) => {
+	res.send(
+		`<p>Phonebook has info for ${persons.length} people</p><br><p>${date}</p>`
 	);
 });
 
-app.get("/api/persons/:id", (request, response) => {
-	const id = request.params.id;
+app.get("/api/persons/:id", (req, res) => {
+	const id = req.params.id;
 	const person = persons.find((person) => person.id === id);
 
 	if (person) {
-		response.json(person);
+		res.json(person);
 	} else {
-		return response.status(404).end();
+		json.status(404).end();
 	}
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-	const id = request.params.id;
-	const person = persons.find((person) => person.id === id);
-	response.status(204).end();
+app.delete("/api/persons/:id", (req, res) => {
+	const id = req.params.id;
+	persons = persons.filter((person) => person.id !== id);
+
+	res.status(204).end();
 });
 
-app.post("/api/persons", (request, response) => {
-	const body = request.body;
-	const nameAlreadyExist = persons.find((person) => person.name === body.name);
+app.post("/api/persons", (req, res) => {
+	const body = req.body;
 
 	if (!body.name || !body.number) {
-		return response.json({ error: "content missing" });
+		return res.status(400).json({ error: "missing content" });
 	}
 
-	if (nameAlreadyExist) {
-		return response.json({ error: "name must be unique" });
+	if (nameAlreadyExist(body.name)) {
+		return res.status(400).json({ error: "Name already exist." });
 	}
 
 	const person = {
-		id: Math.random() * 10000,
 		name: body.name,
 		number: body.number,
+		id: generateId(),
 	};
 
 	persons = persons.concat(person);
-	console.log(persons);
-	response.json(person);
+	res.json(person);
 });
 
 const PORT = 3001;
-app.listen(PORT, () => console.log(`Server is running in ${PORT}`));
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
