eb5a4c1ff97f520dd7712a4be2516e12bd47fc58
Nelman25 <jonelvillaver735@gmail.com>
2024-11-15 04:56:05 +0800

    done exercise 3.13

--- index.js
+++ index.js
@@ -1,104 +1,105 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const morgan = require("morgan");
+const Person = require("./models/mongo");
 
 app.use(express.json());
+app.use(cors());
 app.use(express.static("dist"));
 
 const date = new Date();
 
-let persons = [
-	{
-		id: "1",
-		name: "Arto Hellas",
-		number: "040-123456",
-	},
-	{
-		id: "2",
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-	},
-	{
-		id: "3",
-		name: "Dan Abramov",
-		number: "12-43-234345",
-	},
-	{
-		id: "4",
-		name: "Mary Poppendieck",
-		number: "39-23-6423122",
-	},
-];
+// let persons = [
+//   {
+//     id: "1",
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: "2",
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: "3",
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: "4",
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
 
 const generateId = () => {
-	const randomId = Math.random() * 100;
-	return String(randomId);
+  const randomId = Math.random() * 100;
+  return String(randomId);
 };
 
 const nameAlreadyExist = (name) => {
-	const names = [...persons.map((person) => person.name.trim().toLowerCase())];
-	return names.includes(name.toLowerCase().trim());
+  const names = [...persons.map((person) => person.name.trim().toLowerCase())];
+  return names.includes(name.toLowerCase().trim());
 };
 
 morgan.token("post-data", (req) => {
-	return req.method === "POST" ? JSON.stringify(req.body) : "";
+  return req.method === "POST" ? JSON.stringify(req.body) : "";
 });
+
 app.use(
-	morgan(
-		":method :url :status :res[content-length] - :response-time ms :post-data"
-	)
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :post-data"
+  )
 );
 
-app.get("/api/persons", (req, res) => {
-	res.json(persons);
-});
+app.get("/api/persons", (req, res) =>
+  Person.find({}).then((contacts) => res.json(contacts))
+);
 
 app.get("/info", (req, res) => {
-	res.send(
-		`<p>Phonebook has info for ${persons.length} people</p><br><p>${date}</p>`
-	);
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p><br><p>${date}</p>`
+  );
 });
 
 app.get("/api/persons/:id", (req, res) => {
-	const id = req.params.id;
-	const person = persons.find((person) => person.id === id);
+  const id = req.params.id;
+  const person = persons.find((person) => person.id === id);
 
-	if (person) {
-		res.json(person);
-	} else {
-		json.status(404).end();
-	}
+  if (person) {
+    res.json(person);
+  } else {
+    json.status(404).end();
+  }
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-	const id = req.params.id;
-	persons = persons.filter((person) => person.id !== id);
+  const id = req.params.id;
+  persons = persons.filter((person) => person.id !== id);
 
-	res.status(204).end();
+  res.status(204).end();
 });
 
 app.post("/api/persons", (req, res) => {
-	const body = req.body;
-
-	if (!body.name || !body.number) {
-		return res.status(400).json({ error: "missing content" });
-	}
-
-	if (nameAlreadyExist(body.name)) {
-		return res.status(400).json({ error: "Name already exist." });
-	}
-
-	const person = {
-		name: body.name,
-		number: body.number,
-		id: generateId(),
-	};
-
-	persons = persons.concat(person);
-	res.json(person);
+  // const body = req.body;
+  // if (!body.name || !body.number) {
+  //   return res.status(400).json({ error: "missing content" });
+  // }
+  // if (nameAlreadyExist(body.name)) {
+  //   return res.status(400).json({ error: "Name already exist." });
+  // }
+  // const person = {
+  //   name: body.name,
+  //   number: body.number,
+  //   id: generateId(),
+  // };
+  // persons = persons.concat(person);
+  // res.json(person);
 });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
